98f5817f6b344256bfbe82382f37ccf8fb4c8064
Batto1300 <tommasoricci96@gmail.com>
2020-02-20 19:24:37 +0000

    final fixes

--- read_data.py
+++ read_data.py
@@ -4,23 +4,22 @@ import numpy as np
 
 class Library:
     def __init__(self,n_books_library, signup_process_len, ship_capacity,books):
-        self.n_books_library = n_books_library
-        self.signup_process_len = signup_process_len
-        self.ship_capacity = ship_capacity
+        self.n_books = n_books_library
+        self.signup_process = signup_process_len
+        self.ship = ship_capacity
         self.books = books
 
 def read_csv(file_name = "a_example.txt"):
-    reader = csv.reader(open(csv_file), delimiter=' ')
+    reader = csv.reader(open(file_name), delimiter=' ')
     n_books_total, n_libraries, days_for_scanning = _read_header(reader)
     books_scores = _read_books_scores(reader)
     i = 0
-    libraries = []
+    libraries = np.empty(n_libraries,dtype=Library)
     while(i < n_libraries):
-        i += 1
         n_books_library, signup_process_len, ship_capacity = _read_library(reader)
         books = _read_books_in_library(reader)
-        libraries.append(Library(n_books_library, signup_process_len, ship_capacity, books))
-    libraries = 
+        libraries[i] = Library(n_books_library, signup_process_len, ship_capacity, books)
+        i += 1
     return n_books_total, n_libraries, days_for_scanning, books_scores, libraries
 
 def _read_header(reader):
@@ -28,7 +27,7 @@ def _read_header(reader):
     return int(n_books_total), int(n_libraries), int(days_for_scanning)
 
 def _read_books_scores(reader):
-    return np.array(next(reader), dtype=int)
+    return np.array(list(next(reader)), dtype=int)
 
 def _read_library(reader):
     n_books_library, signup_process_len, ship_capacity = next(reader)
@@ -38,4 +37,4 @@ def _read_books_in_library(reader):
     return set(next(reader))
 
 if __name__ == "__main__":
-    read_csv()
+    res = read_csv()
